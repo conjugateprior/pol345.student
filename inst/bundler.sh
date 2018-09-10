bf0252513c0006d1dce725cd3d380e72af63a817
Will Lowe <conjugateprior@gmail.com>
2018-09-09 21:51:29 -0400

    Put the zips in extdata

--- inst/bundler.sh
+++ inst/bundler.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-rm precept2.zip
-rm precept3.zip
-rm precept4.zip
-rm precept5.zip
-rm precept6.zip
-rm precept7.zip
-rm precept8.zip
-rm precept9.zip
+rm extdata/precept2.zip
+rm extdata/precept3.zip
+rm extdata/precept4.zip
+rm extdata/precept5.zip
+rm extdata/precept6.zip
+rm extdata/precept7.zip
+rm extdata/precept8.zip
+rm extdata/precept9.zip
 
-zip -r --exclude=*.DS_Store* precept1.zip precept1
-zip -r --exclude=*.DS_Store* precept2.zip precept2
-zip -r --exclude=*.DS_Store* precept3.zip precept3
-zip -r --exclude=*.DS_Store* precept4.zip precept4
-zip -r --exclude=*.DS_Store* precept5.zip precept5
-zip -r --exclude=*.DS_Store* precept6.zip precept6
-zip -r --exclude=*.DS_Store* precept7.zip precept7
-zip -r --exclude=*.DS_Store* precept8.zip precept8
-zip -r --exclude=*.DS_Store* precept9.zip precept9
+zip -r --exclude=*.DS_Store* extdata/precept1.zip precept1
+zip -r --exclude=*.DS_Store* extdata/precept2.zip precept2
+zip -r --exclude=*.DS_Store* extdata/precept3.zip precept3
+zip -r --exclude=*.DS_Store* extdata/precept4.zip precept4
+zip -r --exclude=*.DS_Store* extdata/precept5.zip precept5
+zip -r --exclude=*.DS_Store* extdata/precept6.zip precept6
+zip -r --exclude=*.DS_Store* extdata/precept7.zip precept7
+zip -r --exclude=*.DS_Store* extdata/precept8.zip precept8
+zip -r --exclude=*.DS_Store* extdata/precept9.zip precept9
 
